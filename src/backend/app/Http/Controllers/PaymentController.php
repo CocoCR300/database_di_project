8f130a1ffe0df2d0cd77a2114386f7a444b36fb6
CocoCR300 <oscarrojasalvarado@hotmail.com>
2024-05-11 11:45:57 -0600

    Fix inconsistencies in DB creation script and models
    * Add booking items to seed_database file
    * Add PUT API route for payments under the booking route
    * Add more validation rules
    * Show booking payment in booking GET requests

---
+++ src/backend/app/Http/Controllers/PaymentController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Payment;
+use App\Models\User;
+use App\Utils\JsonResponses;
+
+class PaymentController
+{
+    public function index($userName)
+    {
+    }
+}
